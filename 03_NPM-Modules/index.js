4bf3821052a83958f7613f7b3e5809aa674c8a17
Meng Li <lmeng2857@gmail.com>
2024-06-06 11:17:30 +0800

    Add files via upload

---
+++ 03_NPM-Modules/index.js
@@ -0,0 +1,20 @@
+// NPM: Node Package Manager
+// when you install nodeJS, you also installed npm
+// npm has its owm npmjs.com
+// the difference between a node common core module and an npm package is that npm packages are node modules that created by third parties
+
+// npmjs.com ---> documentations--> npm CLI = Command Line Interface
+// CLI is what developers typically use when they're integrating npm into their projects.
+// --- Reading the docs is definitely a big part of being a developer
+
+// npm i nodemon --- it is good to install globally
+
+console.log("testing the scr");
+
+// install a npm package globally, maybe you just want to call it in the command line.
+// ----  npm install/i/add nodemon -g(if add '-g', error occurs)
+// ----  nodemon is a great development package.
+// ----  what nodemon does is it monitors your files and as you save, it automatically restarts the server. so we don't type 'node fileName' in the teminal. it just kind of does it for us.
+
+
+//
